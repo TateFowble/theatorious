bee343b36fd9e63d4fd42ef36c384ca062a5257d
TateFowble <tatefowble@gmail.com>
2020-10-12 20:20:39 -0500

    added

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ var navLinks = {
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '', webtitle: 'The Atorious', link1: navLinks.link1, link2: navLinks.link2, link3: navLinks.link3 });
+  res.render('index', { title: 'At', webtitle: 'The Atorious', link1: navLinks.link1, link2: navLinks.link2, link3: navLinks.link3 });
 });
 
 router.get('/what-we-do', function(req, res, next) {
@@ -19,6 +19,10 @@ router.get('/what-we-do', function(req, res, next) {
 
 router.get('/contact', function (req, res, next) {
   res.render('contact', { link1: navLinks.link1, link2: navLinks.link2, link3: navLinks.link3 });
+});
+
+router.get('/portfolio', function (req, res, next) {
+  res.render('portfolio', { link1: navLinks.link1, link2: navLinks.link2, link3: navLinks.link3 });
 });
 
 router.get('/examples', function (req, res, next) {
